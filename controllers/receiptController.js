7046518fc3924b66a7e717f8e1bf8f9c684ddd18
yusufalp <alpyusuf007@gmail.com>
2024-11-30 11:53:58 -0800

    build endpoints, to process a receipt and generate id, retrieve the points for a receipt by id

--- controllers/receiptController.js
+++ controllers/receiptController.js
@@ -1,3 +1,31 @@
-export const getPointsById = (req, res, next) => {};
+import { v4 as uuidv4 } from "uuid";
 
-export const processReceipt = (req, res, next) => {};
+import Points from "../utils/Points.js";
+import { processedReceipts } from "../data/processedReceipts.js";
+
+export const getPointsById = (req, res, next) => {
+  const { id } = req.params;
+
+  if (!processedReceipts.has(id)) {
+    return res.status(404).json({ error: { message: `Receipt not found` } });
+  }
+
+  const receipt = processedReceipts.get(id);
+
+  const points = new Points(receipt).getPoints();
+
+  res.status(200).json({ points });
+};
+
+export const processReceipt = (req, res, next) => {
+  const receipt = req.body;
+
+  const id = uuidv4();
+
+  const newReceipt = { id, ...receipt };
+
+  processedReceipts.set(id, newReceipt);
+  console.log("processedReceipts :>> ", processedReceipts);
+
+  res.status(201).json({ id });
+};
