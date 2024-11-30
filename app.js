f0d7c60f1709943d7b1f109cce5e780a91bf3ba2
yusufalp <alpyusuf007@gmail.com>
2024-11-30 11:53:17 -0800

    implement the router into the server

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from "express";
 
+import receiptsRouter from "./routers/receiptsRouter.js";
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/receipts", receiptsRouter);
 
 const PORT = 8080;
 
